b0060cf46d88e6e4d8c8221f0b17c8d854c24990
Jalison Sousa <140900161+jalisonsousa@users.noreply.github.com>
2023-08-31 18:42:21 -0400

    Update view-text.php

--- view-text.php
+++ view-text.php
@@ -22,7 +22,7 @@ $content = file_get_contents('notepad.txt');
                     <div class="content">
                         <pre id="preview" style="font-size: 18px;"><?php echo htmlentities($content); ?></pre>
                     </div>
-                    <button id="copyButton" class="button">Copiar Conteúdo</button>
+                    <button id="copyButton" class="button">Copiar texto</button>
                     <button onclick="goToFilesPage()" class="button">Voltar para a Página Inicial</button>
                 </center>
             </div>
