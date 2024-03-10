363ba6c77ea7044d424704ce73b597c6851e8897
Jalison Sousa <sjalison502@gmail.com>
2024-03-10 17:45:04 -0400

    atualização do código

--- js/sweetAlert-view-text.js
+++ js/sweetAlert-view-text.js
@@ -1,21 +1,21 @@
 function copyText(e) {
-  var t = document.createElement("textarea");
-  t.value = e;
-  document.body.appendChild(t);
-  t.select();
-  document.execCommand("copy");
-  document.body.removeChild(t);
-  Swal.fire({
-    title: "copiado com sucesso!",
-    icon: "success",
-    timer: 1500, // Tempo em milissegundos
-    showConfirmButton: false, // Remove o botão de confirmação
-  });
+    var t = document.createElement("textarea");
+    t.value = e;
+    document.body.appendChild(t);
+    t.select();
+    document.execCommand("copy");
+    document.body.removeChild(t);
+    Swal.fire({
+        title: "copiado com sucesso!",
+        icon: "success",
+        timer: 1500, // Tempo em milissegundos
+        showConfirmButton: false // Remove o botão de confirmação
+    });
 }
-document.addEventListener("DOMContentLoaded", function () {
-  var e = document.getElementById("copyButton");
-  var t = document.getElementById("preview");
-  e.addEventListener("click", function () {
-    copyText(t.textContent);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    var e = document.getElementById("copyButton");
+    var t = document.getElementById("preview");
+    e.addEventListener("click", function() {
+        copyText(t.textContent);
+    });
 });
--- view-text.php
+++ view-text.php
@@ -1,43 +1,44 @@
-<?php
-$content = file_get_contents('notepad.txt');
-?>
-<!DOCTYPE html>
-<html lang="pt-BR">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Um bloco de notas simples para usar no computador e copiar texto no celular ou vice-versa, para uso pessoal. ">
-        <link rel="icon" href="icon.png" type="image/icon type">
-        <link rel="stylesheet" type="text/css" href="css/view-text.css">
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script src="https://cdn.jsdelivr.net/gh/jalisonsousa/RemoveBanner/removebanner.js"></script>
-        <title> JSNote Online - Texto</title>
-    </head>
-    <body>
-        <main>
-            <div class="container">
-                <h1>Visualizar Texto</h1>
-                <center>
-                    <div class="content">
-                        <pre id="preview" style="font-size: 18px;"><?php echo htmlentities($content); ?></pre>
-                    </div>
-                    <button id="copyButton" class="button">Copiar texto</button>
-                    <button onclick="goToFilesPage()" class="button">Voltar Página Inicial</button>
-                </center>
-            </div>
-            <div class="footer">
-                <a href="https://github.com/jalisonsousa">
-                    <img src="https://jalisonbr.com/img/jalisonbrLogo.jpg"
-                        alt="JalisonSousa" class="footer-image">
-                </a>
-            </div>
-        </main>
-        <script src="js/sweetAlert-view-text.js"></script>
-        <script>
-        function goToFilesPage() {
-            window.location.href = '\index.html';
-        }
-        </script>
-    </body>
+<?php
+   $content = file_get_contents('notepad.txt');
+   ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Um bloco de notas simples para usar no computador e copiar texto no celular ou vice-versa, para uso pessoal. ">
+      <meta name="keywords" content="jalisonsousa, jalison sousa, jalisonbr, JalisonBR, jalison, sitenote, JSnote, viewtext">
+      <link rel="icon" href="icon.png" type="image/icon type">
+      <link rel="stylesheet" type="text/css" href="css/view-text.css">
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+      <script src="https://cdn.jsdelivr.net/gh/jalisonsousa/RemoveBanner/removebanner2.js"></script>
+      <title> JSNote Online - Texto</title>
+   </head>
+   <body>
+      <main>
+         <div class="container">
+            <h1>Visualizar Texto</h1>
+            <center>
+               <div class="content">
+                  <pre id="preview" style="font-size: 18px;"><?php echo htmlentities($content); ?></pre>
+               </div>
+               <button id="copyButton" class="button">Copiar texto</button>
+               <button onclick="goToFilesPage()" class="button">Voltar Página Inicial</button>
+            </center>
+         </div>
+         <div class="footer">
+            <a href="https://github.com/jalisonsousa">
+            <img src="https://jalisonbr.com/img/jalisonbrLogo.jpg"
+               alt="JalisonSousa" class="footer-image">
+            </a>
+         </div>
+      </main>
+      <script src="js/sweetAlert-view-text.js"></script>
+      <script>
+         function goToFilesPage() {
+             window.location.href = '\index.html';
+         }
+      </script>
+   </body>
 </html>
